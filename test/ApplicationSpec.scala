4495e709ef77f7fc5cf29873390cbe59a69aa72c
tyouteis <tyouteis@gmail.com>
2014-04-14 18:52:54 +0900

    Add unit test cases

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -5,26 +5,39 @@ import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
 
+import org.specs2.specification.{ BeforeExample, AfterExample, Scope }
+
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
 @RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
+class ApplicationSpec extends Specification with BeforeExample {
+  def fakeApp = FakeApplication(
+    additionalConfiguration = inMemoryDatabase())
+  def before = {
+    running(fakeApp) {
+       1 must beEqualTo(1)
+    }
+  }
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
+    "send 303 on a undefined action" in {
+      running(fakeApp) {
+        route(FakeRequest(GET, "/boum")) must beNone
+      }
     }
 
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
+    "render the index page" in {
+      running(fakeApp) {
+        val home = route(FakeRequest(GET, "/tasks")).get
 
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+        status(home) must equalTo(OK)
+        contentType(home) must beSome.which(_ == "text/html")
+        contentAsString(home) must contain ("task(s)")
+      }
     }
   }
 }
--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -4,22 +4,29 @@ import org.junit.runner._
 
 import play.api.test._
 import play.api.test.Helpers._
+import org.specs2.specification.{ BeforeExample, AfterExample, Scope }
 
 /**
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
 @RunWith(classOf[JUnitRunner])
-class IntegrationSpec extends Specification {
+class IntegrationSpec extends Specification with BeforeExample {
+  def fakeApp = FakeApplication(
+    additionalConfiguration = inMemoryDatabase())
+  def before = {
+    running(fakeApp) {
+       1 must beEqualTo(1)
+    }
+  }
 
   "Application" should {
 
     "work from within a browser" in new WithBrowser {
-
-      browser.goTo("http://localhost:" + port)
-
-      browser.pageSource must contain("Your new application is ready.")
-      browser.pageSource must contain("test")
+      running(fakeApp) {
+        browser.goTo("http://localhost:" + port)
+        browser.pageSource must contain("task(s)")
+     }
     }
   }
 }
